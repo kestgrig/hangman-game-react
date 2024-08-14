3c0a9f55cdfe4001d39b96b17ae3028184f7fd00
kestgrig <kestutis.grigenas@gmail.com>
2024-08-14 03:28:14 +0300

    img

--- src/components/hangmanImg/HangmanImg.jsx
+++ src/components/hangmanImg/HangmanImg.jsx
@@ -1,20 +1,23 @@
 import style from './HangmanImg.module.css'
 
 import Imggallows from '/img/imggallows.png'
-// import ImgHead from '/img/1.png';
-// import ImgNeck from '/img/2.png';
-// import ImgHandLeft from '/img/3.png';
-// import ImgHandRight from '/img/4.png';
-// import ImgLegLeft from '/img/5.png';
-// import ImgLegRight from '/img/6.png';
+import ImgHead from '/img/1.png';
+import ImgNeck from '/img/2.png';
+import ImgHandLeft from '/img/3.png';
+import ImgHandRight from '/img/4.png';
+import ImgLegLeft from '/img/5.png';
+import ImgLegRight from '/img/6.png';
 
 
-export function Hangman () {
-
-
-    return (
-        <>
-            <img className={style.gallowsmain} src={Imggallows} alt="imggallows" />
-        </>
-    );
+export function HangmanImg (params) {
+  const imag = [Imggallows, ImgHead, ImgNeck, ImgHandLeft, ImgHandRight, ImgLegLeft, ImgLegRight];
+  return (
+    <div className={style.hangman}
+      <img src={Imggallows} alt= {img} />
+    </div>
+   
+  )
 }
+
+export default HangmanImg;
+
